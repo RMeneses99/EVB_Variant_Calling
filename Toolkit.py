4fb188e2ea7d426658a0082e76597f899889f704
Rui Meneses <rui_manuel99@hotmail.com>
2024-06-03 15:42:29 +0100

    feat: new features

--- Toolkit.py
+++ Toolkit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import os, subprocess, ast, datetime
+import pysam
 import VCSeek_config
 from ancestral_genomes_db import ancestral_genomes
 
-
+#! Must change directory and subdirectory scheme
 #! add a feature that removes the ref folder created by bbsplit
 #! ERRRORS:  hgt = input() - takes folders with no .fq/fq.gz, fastq/fastq.gz on it. You can also force your way through it without using y or n.
 
@@ -61,7 +62,7 @@ def ask_usr(breseq=False):
     return usr_path, fq_list, breseq, co_evo, script_path
 
 
-def pair_by(data_path, search_4_r1='_R1', search_4_r2='_R2'):
+def pair_by(data_path=tuple, search_4_r1='_R1', search_4_r2='_R2'):
     """
     Pairs forward and reverse reads correctly per sample.
 
@@ -95,7 +96,7 @@ def pair_by(data_path, search_4_r1='_R1', search_4_r2='_R2'):
     return fq_pairs
 
 
-def quali_check(data_path):
+def quali_check(data_path=tuple):
     """
     Runs FastQC on the provided data path or input directory and logs the output using MultiQC.
 
@@ -134,7 +135,7 @@ def quali_check(data_path):
     subprocess.run(multiqc_command, check=True, shell=True, capture_output=True, text=True)
 
 
-def pre_processing(data_path, hgt=False):
+def pre_processing(data_path=tuple, hgt=False):
     """
     Preprocesses paired-end FASTQ files.
 
@@ -146,103 +147,113 @@ def pre_processing(data_path, hgt=False):
     Returns:
         tuple: A tuple containing the output directory and a list of processed files.
     """
-
     # Use pair_by function to select pairs
-    fq_pairs = pair_by(data_path)
+    fq_pairs = pair_by(data_path)  # Utilizing a function named pair_by to select pairs of FASTQ files.
 
     # Initialize variables
-    processed_files = []
-    if data_path[2]:
-        output_directory = os.path.join(data_path[4], VCSeek_config.BASE_DIRECTORIES[1], VCSeek_config.SUB_DIRECTORY[1])
-    elif not data_path[2]:
-        output_directory = os.path.join(data_path[4], VCSeek_config.BASE_DIRECTORIES[0], VCSeek_config.SUB_DIRECTORY[1])
+    processed_file_pairs = {}  # A dictionary to store processed file pairs.
+    if data_path[2]:  # Checking if the third element of data_path is True.
+        output_directory = os.path.join(data_path[4], VCSeek_config.BASE_DIRECTORIES[1], VCSeek_config.SUB_DIRECTORY[1])  # Setting the output directory based on conditions.
+    elif not data_path[2]:  # If the third element of data_path is not True.
+        output_directory = os.path.join(data_path[4], VCSeek_config.BASE_DIRECTORIES[0], VCSeek_config.SUB_DIRECTORY[1])  # Setting the output directory based on conditions.
 
     # Iterate over each pair of forward and reverse reads
-    for fq_forward, fq_reverse in fq_pairs.items():
+    for fq_forward, fq_reverse in fq_pairs.items():  # Iterating over each pair of forward and reverse reads.
         # Extract sample tag from forward read filename
-        sample_tag = fq_forward.split('R1')[0]
+        sample_tag = fq_forward.split('R1')[0]  # Extracting a sample tag from the forward read filename.
 
         # Define fastp command
         fastp_command = [
-            'fastp',
-            '-q', '20',
-            '-u', '50',
-            '--dedup', '1',
-            '--detect_adapter_for_pe',
-            '-p', '3',
+            'fastp',  # The command to be executed.
+            '-q', '20',  # Quality filtering threshold.
+            '-u', '50',  # The minimum length of reads to be retained.
+            '--dedup', '1',  # Deduplication level.
+            '--detect_adapter_for_pe',  # Automatically detect adapters for paired-end reads.
+            '-p', '3',  # Number of threads to use.
             '-5',
             '-M', '20',
             '-W', '4',
             '-c',
-            '-i', f'{data_path[0]}/{fq_forward}',
-            '-I', f'{data_path[0]}/{fq_reverse}',
-            '-o', f'{output_directory}/processed_{fq_forward}',
-            '-O', f'{output_directory}/processed_{fq_reverse}',
-            '--html', '/dev/null',
-            '--json', '/dev/null'
+            '-i', f'{data_path[0]}/{fq_forward}',  # Input file for forward reads.
+            '-I', f'{data_path[0]}/{fq_reverse}',  # Input file for reverse reads.
+            '-o', f'{output_directory}/fastp_{fq_forward}',  # Output file for processed forward reads.
+            '-O', f'{output_directory}/fastp_{fq_reverse}',  # Output file for processed reverse reads.
+            '--html', '/dev/null',  # Output HTML report file.
+            '--json', '/dev/null'  # Output JSON report file.
         ]
-        print(f'Running: {" ".join(fastp_command)}')
+        print(f'Running: {" ".join(fastp_command)}')  # Printing the fastp command being executed.
         # Run fastp command
-        subprocess.run(fastp_command, check=True, capture_output=True)
+        subprocess.run(fastp_command, check=True)  # Executing the fastp command.
 
         # Determine reference genomes based on HGT flag
-        ref_genomes = []  # Initialize ref_genomes outside of the conditional blocks
-        if hgt:
-        # Check if any tag from ancestral_genomes is present in the sample name
-            for tag in ancestral_genomes.values():
-                if tag in fq_forward:
-                    ref_genomes.extend([k for k, v in ancestral_genomes.items() if v == tag])
-        else:
-            for tag in data_path[3]:
-                ref_genomes.extend([k for k, v in ancestral_genomes.items() if v == tag])
+        ref_genomes = []  # Initializing a list to store reference genomes.
+        if hgt:  # If hgt is True.
+            # Check if any tag from ancestral_genomes is present in the sample name
+            for tag in ancestral_genomes.values():  # Iterating over values in ancestral_genomes.
+                if tag in fq_forward:  # Checking if the tag is present in the forward read filename.
+                    ref_genomes.extend([k for k, v in ancestral_genomes.items() if v == tag])  # Extending ref_genomes based on conditions.
+        else:  # If hgt is not True.
+            for tag in data_path[3]:  # Iterating over elements in data_path[3].
+                ref_genomes.extend([k for k, v in ancestral_genomes.items() if v == tag])  # Extending ref_genomes based on conditions.
 
-        ref_genomes_str = ','.join(ref_genomes)
+        ref_genomes_str = ','.join(ref_genomes)  # Joining elements of ref_genomes into a string.
 
         # Define bbsplit command
         bbsplit_command = [
-                          f'bbsplit.sh',
-                          f'in1={output_directory}/processed_{fq_forward}',
-                          f'in2={output_directory}/processed_{fq_reverse}',
-                          f'ambig2=best',
-                          f'ref={ref_genomes_str}',
-                          f'basename={output_directory}/{sample_tag}%.fq.gz',
+                          f'bbsplit.sh',  # The command to be executed.
+                          f'in1={output_directory}/fastp_{fq_forward}',  # Input file for forward reads.
+                          f'in2={output_directory}/fastp_{fq_reverse}',  # Input file for reverse reads.
+                          f'ambig2=toss',  # Treatment of ambiguous reads.
+                          f'ref={ref_genomes_str}',  # Reference genomes.
+                          f'basename={output_directory}/{sample_tag}%.fq.gz',  # Base name for output files.
+                          f'outu={output_directory}/{sample_tag}_unmapped.fastq'  # Output file for unmapped reads.
                           ]
         
-        print(f'Running: {" ".join(bbsplit_command)}')
+        print(f'Running: {" ".join(bbsplit_command)}')  # Printing the bbsplit command being executed.
         # Run bbsplit command
-        subprocess.run(bbsplit_command, capture_output=True)
-        for i in range(len(ref_genomes)):
-            ref_genome_name = ref_genomes[i].split('/')[-1]
-            for ext in VCSeek_config.ALLOWED_REF_GENOME_EXTENSIONS:
-                if ext in ref_genome_name:
-                    ref_genome_name = ref_genome_name.split(ext)[0]
+        subprocess.run(bbsplit_command)  # Executing the bbsplit command.
+        for i in range(len(ref_genomes)):  # Iterating over the length of ref_genomes.
+            ref_genome_name = ref_genomes[i].split('/')[-1]  # Extracting the name of the reference genome.
+            for ext in VCSeek_config.ALLOWED_REF_GENOME_EXTENSIONS:  # Iterating over elements in ALLOWED_REF_GENOME_EXTENSIONS.
+                if ext in ref_genome_name:  # Checking if the extension is in the reference genome name.
+                    ref_genome_name = ref_genome_name.split(ext)[0]  # Removing the extension from the reference genome name.
+                       # Define output file names with reference genome name
+                    fq_forward_ref = f"{fq_forward.split('.')[0]}_{ref_genome_name}.fq.gz"  # Output file name for processed forward reads.
+                    fq_reverse_ref = f"{fq_reverse.split('.')[0]}_{ref_genome_name}.fq.gz"  # Output file name for processed reverse reads.
                     # Define bbsplit reformat command
-                    bbsplit_reformat = ['reformat.sh',
-                                       f'in={output_directory}/{sample_tag}{ref_genome_name}.fq.gz',
-                                       f'out1={output_directory}/{fq_forward}',
-                                       f'out2={output_directory}/{fq_reverse}'
+                    bbsplit_reformat = ['reformat.sh',  # The command to be executed.
+                                       f'in={output_directory}/{sample_tag}{ref_genome_name}.fq.gz',  # Input file for reformatting.
+                                       f'out1={output_directory}/split_{fq_forward_ref}',  # Output file for processed forward reads.
+                                       f'out2={output_directory}/split_{fq_reverse_ref}'  # Output file for processed reverse reads.
                                        ]
-#! Right now the two loops are saving in the same files: out1={output_directory}/{fq_forward} and out2={output_directory}/{fq_reverse} I need to fix this.
 
-
-                    print(f'Running: {" ".join(bbsplit_reformat)}')
+                    print(f'Running: {" ".join(bbsplit_reformat)}')  # Printing the bbsplit reformat command being executed.
                     # Run bbsplit reformat command
-                    subprocess.run(bbsplit_reformat, capture_output=True)
+                    subprocess.run(bbsplit_reformat)  # Executing the bbsplit reformat command.
+                    processed_file_pairs[f'{output_directory}/{fq_forward_ref}'] = f'{output_directory}/{fq_reverse_ref}'  # Storing processed file pairs.
 
-        # Print completion message
         print('Preprocessing completed successfully!')
 
-        #! This is not sufficient has if not removed we also include the fastp output files...
-        # Add processed files to list
-        processed_files.extend(os.listdir(output_directory))
+    return  data_path, processed_file_pairs 
 
-    # Return output directory and list of processed files
-    return output_directory, processed_files
 
-#def alignment():
+def pre_alignment(processed_file_pairs=tuple):
+    #for file in processed_file_pairs[1]:
+    return
 
-#def optmized_breseq(data_path):
-    #for 
+
+def filter_sam(sam_file_path, sam_ouput_path):
+    sam_file = pysam.AlignedFile(sam_file_path, "r")
+    output_sam_file = pysam.AlignmentFile(sam_ouput_path, "w", header=sam_file.header) 
+    for read in sam_file:
+        if "MD" in read.get_tags():
+            md_tag = dict(read.get_tags())["MD"]
+            mismatches = sum(1 for c in md_tag if c.isalpha())
+            if mismatches <= 2:
+                output_sam_file.write(read)
+    sam_file.close()
+    output_sam_file.close()
+    #def optmized_breseq(data_path): 
 
 def main():
     ask_usr()
